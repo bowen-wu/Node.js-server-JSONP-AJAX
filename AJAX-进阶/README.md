1ea0e7728b98f008f077e7b7f77b2eb80ec77e6b
bowen-wu <13793248162@163.com>
2018-01-16 17:14:10 +0800

    使用 Promise 规范进行优化

--- AJAX-进阶/README.md
+++ AJAX-进阶/README.md
@@ -8,7 +8,9 @@
 
 4. 实现传入不定个参数的优化。既可以传入一个对象，也可以传入 url + 对象。[代码地址](https://github.com/bowen-wu/Node.js-server-JSONP-AJAX/tree/3ca84506652fa89c4f3854da77420d7f6094e240)。此时由于要使用 arguments ，所以不能使用箭头函数。
 
-5. 优化代码，使用 ES6 解构赋值达到优化代码的目的，并增加 request.setRequestHeader() 功能。[代码地址]()。
-**说明：**如果不实现传入不定个参数，还可以进一步优化。[代码地址]()
+5. 优化代码，使用 ES6 解构赋值达到优化代码的目的，并增加 request.setRequestHeader() 功能。[代码地址](https://github.com/bowen-wu/Node.js-server-JSONP-AJAX/tree/5118accb4c21f0ed2b8da6f2c5677867d238bf51)。
 
+6. 如果不实现传入不定个参数，还可以进一步使用 ES6 解构赋值优化。[代码地址](https://github.com/bowen-wu/Node.js-server-JSONP-AJAX/tree/88436fc2378cec3a814a96cc3260a5de133038da/AJAX-%E8%BF%9B%E9%98%B6)
+
+7. 使用 Promise 规范进行优化，` return new Promise(function(resolve,reject){}) `，成功 === 调用 resolve，失败 === 调用 reject，之后在调用时使用 ` .then ` 方法。[代码地址]()
 
--- AJAX-进阶/js/main.js
+++ AJAX-进阶/js/main.js
@@ -1,32 +1,26 @@
 window.onload = function () {
     let button = document.getElementById('button')
     window.jQuery = function(){}
-    window.jQuery.ajax = function({method,url,body,headers,successFn,errorFn}){
-        // let url
-        // if(arguments.length === 1){
-        //     url = options.url
-        // }else if(arguments.length === 2){
-        //     url = arguments[0]
-        //     options = arguments[1]
-        // }
-        // let {method,body,headers,successFn,errorFn} = options   // ES6 解构赋值 优化代码
-        let request = new XMLHttpRequest()
-        request.open(method, url)
-        for(let key in headers){
-            let value = headers[key]
-            request.setRequestHeader(key,value)
-        }
-        request.onreadystatechange = () => {              
-            if (request.readyState === 4) {                 
-                if (request.status >= 200 && request.status < 300) {  
-                    let obj = window.JSON.parse(request.responseText)
-                    successFn.call(null,obj)
-                } else if (request.status >= 400) {
-                    errorFn.call(null,response)
+    window.jQuery.ajax = function ({ method, url, body, headers }) {
+        return new Promise(function (resolve, reject) {
+            let request = new XMLHttpRequest()
+            request.open(method, url)
+            for (let key in headers) {
+                let value = headers[key]
+                request.setRequestHeader(key, value)
+            }
+            request.onreadystatechange = () => {
+                if (request.readyState === 4) {
+                    if (request.status >= 200 && request.status < 300) {
+                        let obj = window.JSON.parse(request.responseText)
+                        resolve.call(null, obj)
+                    } else if (request.status >= 400) {
+                        reject.call(null, response)
+                    }
                 }
             }
-        }
-        request.send(body)    
+            request.send(body)
+        })    
     }
     window.$ = window.jQuery
  
@@ -40,17 +34,25 @@ window.onload = function () {
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'who': 'bowen'
             },
-            successFn: successFn,
-            errorFn: errorFn
-        })
+        }).then(
+            (event) => {  // event === obj === request.responseText 这是成功时调用的函数
+                console.log('success')
+                console.log(event)
+            },
+            (event) => {  // event === response   这是失败时调用的函数
+                console.log('error')
+                console.log(event)
+            }
+            )
+        // }).then(successFn,errorFn)
     })
 
-    function successFn(event){
-        console.log('success')
-        console.log(event)
-    }
-    function errorFn(event){
-        console.log('error')
-        console.log(event)
-    }
+    // function successFn(event){
+    //     console.log('success')
+    //     console.log(event)
+    // }
+    // function errorFn(event){
+    //     console.log('error')
+    //     console.log(event)
+    // }
 }
